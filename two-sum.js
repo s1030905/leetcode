3eac1f4c652e431efd72302c12309cf29bb5257a
s1030905 <s1030905@outlook.com>
2023-06-14 15:07:19 +0800

    advise two-sum

--- two-sum.js
+++ two-sum.js
@@ -1,19 +1,22 @@
 const cases = {
-  case1: [[2, 7, 11, 15], [9]],
+  case1: [[2, 7, 11, 15], 9],
   case2: [[3, 2, 4], 6],
   case3: [[3, 3], 6]
 }
 
 var twoSum = function (nums, target) {
-  const map = new Map();
+  const dic = {}
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (map.has(complement)) {
-      return [map.get(complement), i];
+    index = target - nums[i]
+
+    // 當dic[index] = 0時不會觸發因為是falsy
+    if (dic[index] >= 0) {
+
+      return [i, dic[index]]
+    } else {
+      dic[nums[i]] = i
     }
-    map.set(nums[i], i);
   }
-  return [];
 };
 
-console.log(twoSum(cases.case1[0], cases.case1[1]))
+console.log(twoSum(cases.case2[0], cases.case2[1]))
